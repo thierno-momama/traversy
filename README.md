b319969c611edd479908a55b29ff5964d648674c
thierno momama <saidsoumah@gmail.com>
2022-11-11 17:13:26 +0100

    JS fundamental

---
+++ README.md
@@ -0,0 +1,32 @@
+# WHAT IS JAVASCRIPT ?
+- Hight level, interpreted programming language
+- Conforms to the ECMAScript specification
+- Multi-paradigm
+- Runs on the client/browser as well as on the server (Node.js)
+# WHY LEARN JAVASCRIPT ?
+- It is the programming language of the browser
+- Build very interactive user interface with frameworks like React
+- Used in building very fast server side and full stack applications
+- Used in mobile development (React Native, NativeScript, ionic)
+- Used in desktop application development (Electron JS)
+# WHAT YOU WILL LEARN IN THIS COURSE
+- Variable & Data Types 
+- Arrays
+- Object Literals
+- Methods for strings, arrays, object, etc
+- Loops - for, while, for...of, forEach, map
+- Conditionals (if, ternary & switch)
+- Functions (nroam & arrow)
+- OOP (prototypes & classes)
+- DOM Selection
+- Events
+- Basic Form Validation
+# FUTHER LEARNING
+- Javascript DOM Crash Course - 4 parts
+- JS OOP Crash Course
+- Hight Order Array Methods Video
+- Async JS Crash Course
+- Fetch API & Ajax Crash Courses
+- Vanilla JS Playlist (Full of small projects)
+- Framework crash courses
+> Let's Jump In!
---
+++ main.js
@@ -0,0 +1,201 @@
+// mdn console
+console.log('Message Log');
+console.warn('Message Warn');
+console.error('Message Error');
+console.info('Message Infos');
+// les variables et leurs types 
+let name = "Thierno";
+let old= 30;
+let oldX = 30.11;
+let x = null;
+let y = undefined;
+let bool = true;
+console.log(`${typeof name} : ${name}`);
+console.log(`${typeof old} : ${old}`);
+console.log(`${typeof oldX} : ${oldX}`);
+console.log(`${typeof x} : ${x}`);
+console.log(`${typeof y} : ${y}`);
+console.log(`${bool} : ${bool}`);
+// les const
+const a = 10;
+console.log(`${a} : ${a}`);
+// les arrays 
+let ar = new Array(12, 23, true, "Pomme");
+console.log(`${ar} : ${ar}`);
+// other arrays
+let fruits = ["pomme", "jus", "orange"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits.unshift("first"));
+console.log(fruits);
+fruits.push("last");
+console.log(fruits);
+console.log(fruits.reverse());
+console.log(fruits.indexOf("pomme"));
+// 
+console.log(name.length);
+// for, while, switch
+for(let i = 0; i < 10; i++ ){
+    console.log(`${i}`);
+}
+let j = 5;
+while(j > 0){
+    console.log("j:",j);
+    j--;
+}
+let color  = "red";
+switch(color){
+    case "red":
+        console.log(color); break;
+    case "yellow":
+        console.log(color); break;
+    default:
+        console.log("Inconnue");
+}
+// forEach, in, of
+for(let i in fruits){
+    console.log(fruits[i]);
+}
+for(let i of fruits){
+    console.log(i);
+}
+// 
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
+// object
+let person = {
+    first: "Thierno",
+    last: "Momama",
+    old: 30,
+    hobbies: ["Prière", "Dev", "Sport"],
+    address: {
+        city: "Vitry",
+        dept: "Vald",
+        cp: 94400
+    }
+}
+console.log(`${typeof person}`);
+const { first, last, hobbies, address: {city}} = person;
+console.log(person);
+hobbies.forEach(function(i){
+    console.log(i);
+});
+
+console.log(city);
+// map, filter, callbacks
+const todos = [
+    {
+        id: 1, 
+        name: "Todo One",
+        isCompleted: true
+    },
+    {
+        id: 2, 
+        name: "Todo Two",
+        isCompleted: false
+    },
+    {
+        id: 3, 
+        name: "Todo Three",
+        isCompleted: true
+    }
+];
+console.log(todos);
+todos.forEach((todo) => {
+    console.log(todo);
+});
+const filtre = todos.filter(function(todo){
+    return todo.isCompleted === true;
+});
+console.log("---------------");
+
+filtre.forEach((todo) => {
+    console.log(todo);
+});
+
+const emps = todos.map((todo) => {
+    return todo.name;
+});
+
+console.log(emps);
+
+const clbs = todos.filter((todo) => {
+    return todo.isCompleted === true; 
+}).map((todo) => {
+    return todo.name;
+});
+console.log(clbs);
+// 
+function adSom(a, b){
+    console.log(a+b);
+}
+adSom(4,3);
+adMon = (a,b) => {
+    return a+b;
+};
+console.log(adMon(5,6));
+// methode 
+function Person(firstName, lastName, oldName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.oldName = new Date(oldName); 
+}
+Person.prototype.getOld = function() {
+    return this.oldName.getFullYear();
+}
+Person.prototype.fullName = function(){
+    return `${this.firstName} : ${this.lastName}`;
+}
+let person1 = new Person("Thierno", "Momama", "11-22-1991");
+console.log(person1);
+console.log(person1.getOld());
+console.log(person1.fullName());
+
+class Humain{
+    
+    constructor(firstName, lastName, oldName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.oldName = new Date(oldName);   
+    }
+    getOld(){
+        return this.oldName.getFullYear();
+    }
+    fullName(){
+        return `${this.firstName} : ${this.lastName}`;
+
+    }
+
+}
+
+const koodev = new Humain("KOO", "DEV", "11-11-2022");
+console.log(koodev);
+console.log(koodev.getOld());
+console.log(koodev.fullName());
+// DOM
+const myForm = document.querySelector('#my-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const msg = document.querySelector(".msg");
+const users = document.querySelector('#users');
+myForm.addEventListener('submit', loadSubmit);
+
+function loadSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === "" || emailInput.value === ""){
+        msg.textContent = "Please enter Name and Email!";
+        msg.style.backgroundColor="red";
+        msg.style.color="white";
+        setTimeout(() => {
+            msg.remove();
+        }, 5000);
+        return;
+    }
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+    users.appendChild(li);
+    // 
+    nameInput.value = '';
+    emailInput.value = '';
+}
